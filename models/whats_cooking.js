a31a6e6073b1482938f1d87b5d8a38b23133c04a
Adam Wenger <adamwenger44@gmail.com>
2020-02-11 21:37:01 -0500

    heroku

--- models/whats_cooking.js
+++ models/whats_cooking.js
@@ -4,10 +4,10 @@
 // This may be confusing but here Sequelize (capital) references the standard library
 var Sequelize = require("sequelize");
 // // sequelize (lowercase) references our connection to the DB.
-// var sequelizeChange = require("../config/connection.js");
+var sequelize = require("../config/connection.js");
 
 // Creates a "Chirp" model that matches up with DB
-var Food = Sequelize.define("food", {
+var Food = sequelize.define("food", {
     food_name: {
         type: Sequelize.STRING,
         allowNull: false,
